dbea0a4caab8b197cbccaae58b53acb2294c3478
Martinho <osvgomes@terra.com.br>
2019-04-04 00:45:55 -0300

    wip - next step: write file with contents

--- textract.js
+++ textract.js
@@ -1,12 +1,23 @@
-const textract = (input_folder, output_folder, single_output = false ) => {
-    console.log(input_folder);
+
+const textract = (input_folder, output_folder, single_output = false) => {
+    const fs = require('fs');
+    const files = fs.readdirSync(`${input_folder}`);
+
+    if (single_output) {
+        // add all output to one sinlge file. name is given as arg 'single_output'
+        files.forEach((file) => {
+            filepath = `./${input_folder}/${file}`
+            const contents = fs.readFileSync(filepath, 'utf8');
+        })
+
+    } else {
+        // create one file for every input. name is the same as original file
+        files.forEach((file) => {
+            filepath = `./${input_folder}/${file}`
+            const contents = fs.readFileSync(filepath, 'utf8');
+        })
+    }    
 }
 
-// change folder names here
-const input_folder = 'input';
-const output_folder = 'output';
-
-// use single_output = true to extract all text to single file
-// else, a new file will be created for each input file
-
-textract(input_folder, output_folder, true);
+// to extract all text to a single file, enter output file name as 'single_output'
+textract(input_folder = 'input', output_folder = 'output', single_output = 'result.txt');
